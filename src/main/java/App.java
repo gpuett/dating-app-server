f28d970e1be86854c8f339c9b748c907762c4850
Garnett Puett, Matt Nardoni and semhar kidane <garnett+matt+semhar@>
2018-06-19 08:43:50 -0700

    changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -84,22 +84,22 @@ public class App {
             return "{\"message\":\"User deleted\"}";
         });
 
-        options("/*", (request, response) -> {
-            String accessControlRequestHeaders = request
-                    .headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers",
-                        accessControlRequestHeaders);
-            }
-
-            String accessControlRequestMethod = request
-                    .headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods",
-                        accessControlRequestMethod);
-            }
-            return "OK";
-        });
+//        options("/*", (request, response) -> {
+//            String accessControlRequestHeaders = request
+//                    .headers("Access-Control-Request-Headers");
+//            if (accessControlRequestHeaders != null) {
+//                response.header("Access-Control-Allow-Headers",
+//                        accessControlRequestHeaders);
+//            }
+//
+//            String accessControlRequestMethod = request
+//                    .headers("Access-Control-Request-Method");
+//            if (accessControlRequestMethod != null) {
+//                response.header("Access-Control-Allow-Methods",
+//                        accessControlRequestMethod);
+//            }
+//            return "OK";
+//        });
 
 
 
